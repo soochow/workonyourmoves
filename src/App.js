0b151677e230a6dc9fce3787520cfa9f70dc77a6
Shuqiao Song <shuqiao@shuqiaos-delorean.local>
2017-03-16 21:02:45 -0700

    show embedded intro video

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Learn from Youtube</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          LOOK AT THIS ADORABLE DOG VIDEO.
         </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/GYrN1f6jGuk" frameborder="0" allowfullscreen></iframe>
       </div>
     );
   }
