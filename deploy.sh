4db60f4298a7e3f47c00bf65a569f596dcc488fa
Mladen Bajic <mladen.bajic@gmail.com>
2017-03-17 00:45:00 -0700

    Create a deploy script to automate building and deploying to cf

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# exit if any command fails
+set -e
+
+# build
+npm run build
+
+touch build/Staticfile
+
+# deploy to cf
+pushd build/
+cf push -b staticfile_buildpack learnfromyoutube
+popd
