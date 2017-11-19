3d54e2e7691a6413fa76cc23173f006e280559e1
Mladen Bajic <mladen.bajic@gmail.com>
2017-11-19 15:57:40 -0800

    Open social links in new tab

--- src/SocialLinks.js
+++ src/SocialLinks.js
@@ -6,13 +6,13 @@ class SocialLinks extends Component {
     render() {
         return (
             <div className="SocialLinks">
-                <a href="https://www.instagram.com/workonyourmoves/">
+                <a href="https://www.instagram.com/workonyourmoves/" target="_blank">
                     <img className="SocialLink" src="instagramicon.png" alt="Work On Your Moves Instagram Page"/>
                 </a>
-                <a href="https://www.facebook.com/workonyourmoves">
+                <a href="https://www.facebook.com/workonyourmoves" target="_blank">
                     <img className="SocialLink" src="facebookicon.png" alt="Work On Your Moves Facebook Page"/>
                 </a>
-                <a href="https://tinyletter.com/workonyourmoves">
+                <a href="https://tinyletter.com/workonyourmoves" target="_blank">
                     <img className="SocialLink" src="emailicon.png" alt="Email Work On Your Moves"/>
                 </a>
             </div>
